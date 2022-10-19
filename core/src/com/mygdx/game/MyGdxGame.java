585ef6b19036e3ac958800209487edbbca7b0065
eduardowna2020 <eduardowna2020@yandex.ru>
2022-10-19 19:49:17 +0300

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 1, 1, 1);
+
+		float x = Gdx.input.getX() - img.getWidth()/2;
+		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2 ;
+
+
+		batch.begin();
+		batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(img, x, y);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
